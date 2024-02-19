6fdc584c834866cda2f7ea9f5f1b93a4fe756331
Dmarzzucco <drrkermazyxokv@gmail.com>
2024-02-19 12:29:21 -0300

    First-fet

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+    return (
+        <div className="home">
+            <h1>Home</h1>
+            {/* <Link to="/about">About</Link> */}
+        </div>
+    )
+}
+
+export default Home;
+
