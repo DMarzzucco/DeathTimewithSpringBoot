45d13efaf02be222e5c5f32be9582088b1f68664
DMarzzucco <123565631+DMarzzucco@users.noreply.github.com>
2024-03-22 12:28:29 -0300

    Update Tc.jsx

--- src/page/Tc.jsx
+++ src/page/Tc.jsx
@@ -4,7 +4,7 @@ function Tc() {
         <main className='TcMain'>
             <section className=" h-All flex justify-start items-center w-full p-2 font-jet">
                 <div className="w-tre justify-start items-center m-10">
-                    <h1 className="text-60 mini3:text-30">MY TCHNOLOGIES</h1>
+                    <h1 className="text-60 mini3:text-30">MY TECHNOLOGIES</h1>
                     <p className="text-Gr">
                         Technologies that will be implemented at this time, until the last update of this page. <br />
                         Last update: 12-03-24
